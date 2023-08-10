f0aca60d6abdd88f2148f17b860cd4c7d666b2fe
majesty-01 <moheethasan4@gmail.com>
2023-08-10 20:44:13 +0600

    initial setup done

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-red-700">Hello world!</div>,
+  },
+]);
